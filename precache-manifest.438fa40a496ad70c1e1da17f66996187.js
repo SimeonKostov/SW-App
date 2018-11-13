add61dc27671f7a0d5eab7ea9b9e5bf151f2dfad
Simeon Kostov <moniak40@gmail.com>
2018-11-13 21:29:09 +0200

    Updates

---
+++ precache-manifest.438fa40a496ad70c1e1da17f66996187.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "93895aa209ef27ff8ae24b3f6421834a",
+    "url": "/SW-app/static/media/logo.93895aa2.png"
+  },
+  {
+    "revision": "33f700110d8d2e206c59265084efea42",
+    "url": "/SW-app/static/media/bryan-goff-395922-unsplash.33f70011.jpg"
+  },
+  {
+    "revision": "d8524c361b789486a430",
+    "url": "/SW-app/static/js/runtime~main.d8524c36.js"
+  },
+  {
+    "revision": "a5b0c21cae23f89c208b",
+    "url": "/SW-app/static/js/main.a5b0c21c.chunk.js"
+  },
+  {
+    "revision": "1bb31c8436cff6ab56e8",
+    "url": "/SW-app/static/js/1.1bb31c84.chunk.js"
+  },
+  {
+    "revision": "a5b0c21cae23f89c208b",
+    "url": "/SW-app/static/css/main.70ab47e8.chunk.css"
+  },
+  {
+    "revision": "1bb31c8436cff6ab56e8",
+    "url": "/SW-app/static/css/1.db6deeda.chunk.css"
+  },
+  {
+    "revision": "a79094af556bd81eac7c3da498f3fe81",
+    "url": "/SW-app/index.html"
+  }
+];
